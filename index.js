82e4e2bcaa326cc5ede21f558c9faae5a7ecd81e
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2023-02-15 19:00:07 +0000

    ok

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('calculate').addEventListener('click',function(){
+    const income = parseInt(document.getElementById('income').value);
+    const food = parseInt(document.getElementById('food').value);
+    const rent = parseInt(document.getElementById('rent').value);
+    const other = parseInt(document.getElementById('other').value);
+    
+})
